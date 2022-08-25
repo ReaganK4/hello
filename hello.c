5f60ffad7f5962987501be482f21264b39675933
Reagan Kahla <rkahla2@huskers.unl.edu>
2022-08-25 16:24:21 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello!! I'm Reagan Kahla!\n");
+    printf("Major is Computer Engineering!\n");
+
+    return 0;
+}
