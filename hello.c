c1456d91067778d61d1422d25095ef6b1cd74f2b
clancyjones <cjones190@huskers.unl.edu>
2022-08-30 03:34:21 +0000

    added Clancy name and major

--- hello.c
+++ hello.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv) {
 
     printf("Hello!! I'm Reagan Kahla!\n");
     printf("Major is Computer Engineering!\n");
+    printf("Hello! My name is Clancy Jones.\n");
+    printf("My major is Computer Science.\n");
 
     return 0;
 }
